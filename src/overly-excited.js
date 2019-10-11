a75866c6be240190cec7d0803cce29cda876bcca
Ryan Bishop <rdbishop06@gmail.com>
2019-10-11 08:21:28 -0500

    Function passed spec

--- src/overly-excited.js
+++ src/overly-excited.js
@@ -13,8 +13,9 @@ function addExcitement (theWordArray) {
 
     for (let i = 0; i < theWordArray.length; i++) {
         // Concatenate the new word onto buildMeUp
-
+        buildMeUp += `${theWordArray[i]} `;
         // Print buildMeUp to the console
+        console.log(buildMeUp);
     }
 
 }
