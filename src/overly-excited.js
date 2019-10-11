a74f42eb433393cc765892f03f4c0223f5d23352
Ryan Bishop <rdbishop06@gmail.com>
2019-10-11 08:43:53 -0500

    Function matches spec

--- src/overly-excited.js
+++ src/overly-excited.js
@@ -1,24 +1,22 @@
 // Create an array that contains the words in the sentence
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
-function addExcitement (theWordArray, string="!") {
-
+   function addExcitement (theWordArray, string="!", repeat=1) {
+    /* 
+    Add a new argument to the function so that a developer can specify 
+    how many times the special character should be added.
+    */
+       
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
-
+    let repeatChar = ""
+    for (let i = 0; i < repeat; i++){
+        repeatChar += string;
+    }
     for (let i = 0; i < theWordArray.length; i++) {
-        /* 
-        Add a new argument to the function so that a developer can specify 
-        which character should be displayed instead of it always being an exclamation point.
-         */
         // Concatenate the new word onto buildMeUp
         if ((i + 1) % 3 === 0){
-            buildMeUp += `${theWordArray[i]}${string} `
+            buildMeUp += `${theWordArray[i]}${repeatChar} `;
         }
         else {
             buildMeUp += `${theWordArray[i]} `;
@@ -26,11 +24,14 @@ function addExcitement (theWordArray, string="!") {
         // Print buildMeUp to the console
         console.log(buildMeUp);
     }
-
+    
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence) // no second argument, defaults to "!"
+// addExcitement(sentence) // no second argument, defaults to "!"
 
 // I want to use a question mark
-addExcitement(sentence, "?") // second argument declared, will use instead of default "!"
+// addExcitement(sentence, "?") // second argument declared, will use instead of default "!"
+
+// I want to use a 4 asterisks
+addExcitement(sentence, "*", 4);
